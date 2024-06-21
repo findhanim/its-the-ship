92e81af0f9f03ac4c0c741273de6b81cb5d787d4
findhanim <work.nazreenhanim@gmail.com>
2024-06-22 01:19:07 +0800

    Updated sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ import headerCollage from '../assets/images/png/header-collage.png'
 import headerPhotoburn from '../assets/images/png/header-photoburn.png'
 import headlinersTitle from '../assets/images/image/headliners-title.png'
 import headlinersFrame from '../assets/images/png/headliners-frame.png'
+import thumbnail from '../assets/images/image/thumbnail-dummy.png'
+import dividerDrip from '../assets/images/png/divider-drip.png'
 
 const IndexPage = () => {
   return (
@@ -23,16 +25,16 @@ const IndexPage = () => {
             <div className="col-lg-5 d-flex justify-content-lg-start justify-content-center">
               <img src={headerIts} height="297px" alt="header-its" />
             </div>
-            <div className="col-lg-7 justify-content-center text-center">
+            <div className="col-lg-7 text-center">
               <p className="t1 mt-lg-0 mt-5">Deeper, Darker, Dirtier</p>
               <h2>
                 Asia's largest festival at sea returns with our spookiest
                 edition.
               </h2>
-              <div>
-                <p className="t3 red-tag">Singapore | 1-3 Nov 2023</p>
-                <Button>Book A Cabin</Button>
+              <div className="d-flex justify-content-center">
+                <p className="t3 red-tag mb-4">Singapore | 1-3 Nov 2023</p>
               </div>
+              <Button>Book A Cabin</Button>
             </div>
           </header>
           {/* Introduction */}
@@ -79,8 +81,140 @@ const IndexPage = () => {
               />
             </div>
             <p className="t2 text-center">With many more to be announced</p>
-
-            <div></div>
+          </section>
+          {/* What To Expect */}
+          <section className="py-5">
+            <div className="d-lg-flex justify-content-between align-items-center text-danger">
+              <h2>What to expect on board</h2>
+              <Button>Book A Cabin</Button>
+            </div>
+            {/* Tabs */}
+            <div class="d-flex align-items-start">
+              <div
+                class="nav flex-column nav-pills me-3"
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation="vertical"
+              >
+                <button
+                  class="nav-link active"
+                  id="v-pills-home-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#v-pills-home"
+                  type="button"
+                  role="tab"
+                  aria-controls="v-pills-home"
+                  aria-selected="true"
+                >
+                  Entertainment
+                </button>
+                <button
+                  class="nav-link"
+                  id="v-pills-profile-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#v-pills-profile"
+                  type="button"
+                  role="tab"
+                  aria-controls="v-pills-profile"
+                  aria-selected="false"
+                >
+                  Profile
+                </button>
+                <button
+                  class="nav-link"
+                  id="v-pills-messages-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#v-pills-messages"
+                  type="button"
+                  role="tab"
+                  aria-controls="v-pills-messages"
+                  aria-selected="false"
+                >
+                  Messages
+                </button>
+                <button
+                  class="nav-link"
+                  id="v-pills-settings-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#v-pills-settings"
+                  type="button"
+                  role="tab"
+                  aria-controls="v-pills-settings"
+                  aria-selected="false"
+                >
+                  Settings
+                </button>
+              </div>
+              <div class="tab-content" id="v-pills-tabContent">
+                <div
+                  class="tab-pane fade show active"
+                  id="v-pills-home"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-home-tab"
+                >
+                  ...
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="v-pills-profile"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-profile-tab"
+                >
+                  ...
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="v-pills-messages"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-messages-tab"
+                >
+                  ...
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="v-pills-settings"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-settings-tab"
+                >
+                  ...
+                </div>
+              </div>
+            </div>
+          </section>
+          {/* Previous Reels */}
+          <section className="py-5">
+            <h3 className="d-lg-flex justify-content-center pb-5">
+              I KNOW WHAT YOU DID LAST SAILINGS…
+            </h3>
+            <div className="d-lg-flex">
+              <div class="row gx-4">
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+              </div>
+            </div>
+            <div className="d-lg-flex my-4">
+              <div class="row gx-4">
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+                <div className="col-lg-4">
+                  <img src={thumbnail} width="100%" alt="header-its" />
+                </div>
+              </div>
+            </div>
+            <div className="d-lg-flex justify-content-center">
+              <Button>Book A Cabin</Button>
+            </div>
           </section>
           {/* Cabins */}
           <section>
@@ -112,6 +246,11 @@ const IndexPage = () => {
             </div>
           </section>
         </div>
+        {/* Past Lineups */}
+        <section className="bg-red d-lg-flex justify-content-center py-5">
+          <h2 className="mt-4">Past Lineups</h2>
+        </section>
+        <img src={dividerDrip} width="100%" alt="divider-drip" />
       </div>
     </main>
   )
