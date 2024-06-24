6a183db66d55880b1066691a38cb6dc25d27b173
findhanim <work.nazreenhanim@gmail.com>
2024-06-24 21:03:10 +0800

    Updated fonts import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
-import "./src/styles/global.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '@popperjs/core/dist/umd/popper.min.js'
+// Global Styling
+import './src/styles/global.css'
+// Fonts
+import './src/assets/fonts/fonts.css'
